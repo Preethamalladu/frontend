f74023e4e9cf194b577846b397d30e06e34f1f9f
ayesh <ayeshreddy227@gmail.com>
2019-12-10 13:33:53 -0500

    create Restaurant page to be integrated with the homepage

---
+++ uberEats/src/app/create-restaurant/create-restaurant.component.ts
@@ -0,0 +1,99 @@
+import { Component, OnInit } from '@angular/core';
+import { RestaurantAdminService } from '../restaurant-admin.service';
+import {ActivatedRoute} from '@angular/router';
+@Component({
+  selector: 'app-create-restaurant',
+  templateUrl: './create-restaurant.component.html',
+  styleUrls: ['./create-restaurant.component.scss']
+})
+export class CreateRestaurantComponent implements OnInit {
+  // menu = [{"name":"abc","category":"abc","isavalable":true,"price":"12$","edit":false},{"name":"abcd","category":"abc","isavalable":true,"price":"12$","edit":false},{"name":"abcde","category":"abc","isavalable":true,"price":"12$","edit":false},{"name":"abc","category":"abc","isavalable":true,"price":"12$","edit":false},{"name":"abc","category":"abc","isavalable":true,"price":"12$","edit":false}];
+  menu=[]
+  restaurant_id = ""
+  menutemplate = {"name":"a","category":"","isavalable":false,"price":"","edit":true};
+  restaurantdata = {}
+  constructor(private restaurantAdminService:RestaurantAdminService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.restaurant_id = this.route.snapshot.paramMap.get('id');
+    console.log(this.restaurant_id);
+    if(this.restaurant_id){
+      this.restaurantAdminService.getRestaurantByID(this.restaurant_id).subscribe((data: any[])=>{
+        this.restaurantdata = data;
+        this.restaurantdata['location'] = this.restaurantdata['loc']['coordinates'].join(',')
+        console.log(this.restaurantdata);
+      })
+      this.getMenu();
+    }
+  }
+  getMenu(){
+    this.restaurantAdminService.getMenuByRestaurant(this.restaurant_id).subscribe((data: any[])=>{
+      this.menu = data;
+      console.log(this.menu);
+    })
+  }
+  check(){
+    console.log(this.menu);
+  }
+  changeListener(files: FileList){
+    console.log(files);
+    if(files && files.length > 0) {
+       let file : File = files.item(0); 
+         console.log(file.name);
+         console.log(file.size);
+         console.log(file.type);
+         let reader: FileReader = new FileReader();
+         reader.readAsText(file);
+         reader.onload = (e) => {
+            let csv: string = reader.result as string;
+            console.log(csv);
+         }
+      }
+  }
+  gg(m){
+    var e = window.event;
+    e.preventDefault();
+    console.log(this.menu);
+  }
+  trackByFn(index, item) {
+    return index;
+}
+  createRestaurant(){
+    console.log(this.restaurantdata)
+    this.restaurantdata['loc'] = {
+      type:  "Point",
+      coordinates: this.restaurantdata['location'].split(",")
+    };
+    console.log(this.restaurantdata)
+    this.restaurantAdminService.createRestaurant(this.restaurantdata).subscribe((data: any[])=>{
+      console.log(data);
+      this.restaurant_id = data["_id"];
+    })  
+  }
+  createMenu(){
+    
+    // this.getMenu();
+    this.menu.splice(0,0,this.menutemplate);
+    // console.log(this.menu);
+  }
+  edit(m){
+    m['edit']=!m['edit'];
+  }
+  update(m){
+    m['restaurant_id'] = this.restaurant_id;
+    if("_id" in m){
+      this.restaurantAdminService.editMenu(m['_id'],m).subscribe((data: any[])=>{
+        this.getMenu();
+        
+      })
+    }
+    else{
+      this.restaurantAdminService.createMenu(m).subscribe((data: any[])=>{
+        this.getMenu();
+      })
+    }
+  }
+  delete(m){
+    
+  }
+}
