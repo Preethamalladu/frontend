b3cd38907f2c514f500c5620fbb64bced058f4ad
Nithinmuvva <muvva.n@husky.neu.edu>
2019-12-11 01:47:13 -0500

    App component chnages

--- uberEats/src/app/app-routing.module.ts
+++ uberEats/src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './Forms/login/login.component';
+import { RegisterLoginComponent } from './Forms/reg-customer/reg-customer.component';
+import { RestaurantRegisterComponent } from './Forms/restaurant-reg/restaurant-reg.component';
+import { AdminLoginComponent} from './Forms/login-admin/login-admin.component';
+import { RegisterAdminComponent} from './Forms/reg-admin/reg-admin.component';
+import { HomeComponent } from './Home/home/home.component';
 
+//import { DriverRegisterComponent } from './Forms/driver-register/driver-register.component';
+//import { DriverLoginComponent} from './Forms/driver-login/driver-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterLoginComponent },
+  { path: 'restuarant/register', component: RestaurantRegisterComponent },
+  { path: 'admin/register', component: RegisterAdminComponent },
+  { path: 'admin', component: AdminLoginComponent },
+  { path: 'home', component: HomeComponent },
+  //{ path: 'driver/register', component: DriverRegisterComponent },
+  //{ path: 'driver/login', component: DriverLoginComponent },
+  { path: '**', redirectTo: '#' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- uberEats/src/app/app.module.ts
+++ uberEats/src/app/app.module.ts
@@ -1,18 +1,62 @@
+import {platformBrowser} from '@angular/platform-browser';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LoginModule } from './Forms/login/login.module';
+import { RegisterModule } from './Forms/reg-customer/reg-customer.module';
+import { RestuarantRegisterModule } from './Forms/restaurant-reg/restaurant-reg.module';
+import { AdminLoginModule } from './Forms/login-admin/login-admin.module';
+import { RegisterAdminModule } from './Forms/reg-admin/reg-admin.module';
+
+import { RegisterCustomerService } from './register-customer.service';
+import { AlertService } from '@app/core/Alert.service';
+import { RestaurantService } from './restaurant-service.service';
+import { AlertComponent } from './Directives/alert/alert.component';
+
+import { AgmCoreModule } from '@agm/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeModule } from './Home/home/home.module';
+//import { DriverRegisterModule } from './Forms/driver-register/driver-register.module';
+//import { DriverLoginModule } from './Forms/driver-login/driver-login.module';
+//import { RestRegisterComponent } from './Forms/rest-login/rest-login.component';
+
+//import { LoginComponent } from './Forms/login/login.component';
+//import { RestaurantLoginComponent } from './Forms/restaurant-login/restaurant-login.component';
+
+//import { Customer } from './Model/Customer.model';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertComponent,
+    //DriverLoginModule,
+   // RestRegisterComponent,
+    //LoginComponent,
+   // RestaurantLoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    LoginModule,
+    RegisterModule,
+    AdminLoginModule,
+    RegisterAdminModule,
+    HomeModule,
+    //DriverRegisterModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBI-fMrs51j5nRg0fw6xLsMMSljUNnOkfo'
+    }),
+    NoopAnimationsModule
   ],
-  providers: [],
+  providers: [RegisterCustomerService, AlertService, RestaurantService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
