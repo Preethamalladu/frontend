afdc88d8941b03d4e8ecec6b7c115865353dcff3
sai preetham reddy alladu <alladu.s@husky.neu.edu>
2019-12-10 19:44:22 -0500

    adding module.ts

--- uberEats/src/app/app.module.ts
+++ uberEats/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomepageCustomerComponent } from './homepage-customer/homepage-customer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { RestaurantHomeMenuComponent } from './restaurant-home-menu/restaurant-home-menu.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageCustomerComponent,
+    RestaurantComponent,
+    RestaurantHomeMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
