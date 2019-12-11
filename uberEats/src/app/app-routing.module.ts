aca0db63879434267370739718c9669cb82e0904
ayesh <ayeshreddy227@gmail.com>
2019-12-11 00:56:58 -0500

    pushing routing module

--- uberEats/src/app/app-routing.module.ts
+++ uberEats/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { HomepageAdminComponent } from '../app/homepage-admin/homepage-admin.component';
+import { CreateRestaurantComponent } from '../app/create-restaurant/create-restaurant.component';
+import { RestaurantPageAdminComponent } from '../app/restaurant-page-admin/restaurant-page-admin.component';
+const routes: Routes = [{ path: 'admin/homepage', component: HomepageAdminComponent,pathMatch:'full' },{ path: 'admin/createrestaurant', component: CreateRestaurantComponent,pathMatch:'full' },{ path: 'admin/restaurant/:id', component: RestaurantPageAdminComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- uberEats/src/app/app.module.ts
+++ uberEats/src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageAdminComponent } from './homepage-admin/homepage-admin.component';
+import { CreateRestaurantComponent } from './create-restaurant/create-restaurant.component';
+import { EditRestaurantAdminComponent } from './edit-restaurant-admin/edit-restaurant-admin.component';
+import { FormsModule } from '@angular/forms';
+import { RestaurantPageAdminComponent } from './restaurant-page-admin/restaurant-page-admin.component';
+import { RestaurantOrdersAdminComponent } from './restaurant-orders-admin/restaurant-orders-admin.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RestaurantAnalyticsAdminComponent } from './restaurant-analytics-admin/restaurant-analytics-admin.component';
+import {MatInputModule} from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageAdminComponent,
+    CreateRestaurantComponent,
+    EditRestaurantAdminComponent,
+    RestaurantPageAdminComponent,
+    RestaurantOrdersAdminComponent,
+    RestaurantAnalyticsAdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
