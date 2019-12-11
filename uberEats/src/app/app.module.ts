7da1a6945de3c09838d5f1897b569aa4ce9130e4
System Administrator <root@Imperio.local>
2019-12-10 19:52:07 -0500

    changes in routes and package.json

--- uberEats/src/app/app.module.ts
+++ uberEats/src/app/app.module.ts
@@ -3,14 +3,77 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CustomerProfileComponent } from './profile/customer-profile/customer-profile.component';
+import { RestaurantProfileComponent } from './profile/restaurant-profile/restaurant-profile.component';
+import { RouterModule } from '@angular/router';
+import { OrdersComponent } from './order/orders/orders.component';
+import { FormsModule } from '@angular/forms';
+import { OrderHistoryComponent } from './orderHistory/order-history/order-history.component';
+import { PaymentComponent } from './payment/payment/payment.component';
+import { SuccesspaymentComponent } from './payment/successpayment/successpayment.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarcompComponent } from './navbarcart/navbarcomp/navbarcomp.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerProfileComponent,
+    RestaurantProfileComponent,
+    CustomerProfileComponent,
+    RestaurantProfileComponent,
+    OrdersComponent,
+    OrderHistoryComponent,
+    PaymentComponent,
+    SuccesspaymentComponent,
+    NavbarcompComponent
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    MatToolbarModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: 'profile/customer/:customerID', 
+        component: CustomerProfileComponent,
+        pathMatch:"full"
+      },
+      {
+        path: 'profile/restaurant/:customerID', 
+        component: RestaurantProfileComponent,
+        pathMatch:"full"
+      },
+      {
+        path: 'orderSummary', 
+        component: OrdersComponent,
+        pathMatch:"full",
+        data: [{ name: 'Heroes List' },{ name: 'Heroes List' },{ name: 'Heroes List' },{ name: 'Heroes List' }]
+      },
+      {
+        path: 'orderHistory', 
+        component: OrderHistoryComponent,
+        pathMatch:"full"
+        // state: [{ name: 'Heroes List' },{ name: 'Heroes List' },{ name: 'Heroes List' },{ name: 'Heroes List' }]
+      },
+      {
+        path: 'payment', 
+        component: PaymentComponent,
+        pathMatch:"full"
+        // state: [{ name: 'Heroes List' },{ name: 'Heroes List' },{ name: 'Heroes List' },{ name: 'Heroes List' }]
+      },
+      {
+        path: 'success', 
+        component: SuccesspaymentComponent,
+        pathMatch:"full"
+        // state: [{ name: 'Heroes List' },{ name: 'Heroes List' },{ name: 'Heroes List' },{ name: 'Heroes List' }]
+      }
+
+    
+    ]),
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
